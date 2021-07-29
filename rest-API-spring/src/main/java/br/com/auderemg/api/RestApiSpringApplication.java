ca99d4b7bee6dbffbef5c9cc3c3167a17c71c754
Messias Martins <messiasmartins@outlook.com>
2021-07-29 01:52:35 -0300

    API Base funcionando

---
+++ rest-API-spring/src/main/java/br/com/auderemg/api/RestApiSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.auderemg.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSpringApplication.class, args);
+	}
+
+}
---
+++ rest-API-spring/src/test/java/br/com/auderemg/api/RestApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.auderemg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
